e53d360fb418b4cf4ffb4020b661e7f91e10a92f
ChristinaWebDev <web.angel1993@gmail.com>
2020-02-27 20:11:05 -0800

    git commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,7 +15,25 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
-        self.save()
+        self.save()    
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approve_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comment = True
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approve_comment==True)
+
+    def __str__(self):
+        return self.text
+    
